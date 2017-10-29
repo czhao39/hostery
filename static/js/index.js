6ee473c1b7796ca6d9813c1fac4bfbb860080695
Haicheng Charles Zhao <czhao39@gmail.com>
2017-10-29 19:10:39 -0400

    Make search form submittable with loading text and spinner

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#search-form").submit(function(event) {
+        start_preloader();
+    })
+});
+
+
+function start_preloader() {
+    $(".preloader-wrapper").addClass("active");
+    $("#loading-text").fadeIn();
+}
