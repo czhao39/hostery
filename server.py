2183ef0a53a00b8b65746e6d0fd3b90f3451aeb4
Haicheng Charles Zhao <czhao39@gmail.com>
2017-10-29 22:23:28 -0400

    Create basic data page with embedded Google Map

--- server.py
+++ server.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
 
 from flask import *
+import requests
+import traceback
 import secret
 import data_processing
-import traceback
 
 
 app = Flask(__name__, static_url_path="")
 app.secret_key = secret.SECRET_KEY
 
 
+@app.template_filter()
+def format_money(m):
+    return "${:,.2f}".format(m)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/data")
 def data():
     try:
         lat = float(request.args.get("latitude"))
         lng = float(request.args.get("longitude"))
+        r = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params = {
+            "latlng": "{},{}".format(lat, lng),
+            "key": secret.GMAPS_API_KEY,
+        }).json()
+        print(r)
+        if r["results"]:
+            place = r["results"][0]
+        else:
+            place = {"formatted_address": None, "place_id": None}
+
         context = {
+            "gmaps_api_key": secret.GMAPS_API_KEY,
             "weekly_avg_income": data_processing.get_weekly_avg_income(lat, lng),
             "max_bookings_price": data_processing.get_max_bookings_price(lat, lng),
+            "formatted_address": place["formatted_address"],
+            "place_id": place["place_id"],
         }
         return render_template("data.html", **context)
     except:
