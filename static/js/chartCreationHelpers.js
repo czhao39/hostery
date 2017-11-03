e1d0e56c474c447378d576c465bd102c983504fb
Haicheng Charles Zhao <czhao39@gmail.com>
2017-11-02 23:10:50 -0400

    Consolidate chart creation functions into one file

--- static/js/chartCreationHelpers.js
+++ static/js/chartCreationHelpers.js
@@ -1,4 +1,9 @@
 /*
+ * Functions for creating the charts.
+ */
+
+
+/*
  * Create the neighborhood radar chart
  */
 function create_neighborhood_chart(data) {
@@ -77,6 +82,46 @@ function create_price_distribution_chart(data) {
                 categoryPercentage: 1.0,
             }],
         },
+    };
+
+    let chart = new Chart(context, {
+        type: "bar",
+        data: data,
+        options: options,
+    });
+}
+
+/*
+ * Create the Average Price vs. Neighborhood bar chart
+ */
+function create_price_vs_neighborhood_chart(data) {
+    let context = $("#price_vs_neighborhood_chart");
+
+    let options = {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    autoSkip: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    // Prepend a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return '$' + value;
+                    }
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: "Average Prices in Most Popular Neighborhoods",
+            fontSize: 28,
+            fontStyle: "normal"
+        }
     };
 
     let chart = new Chart(context, {
