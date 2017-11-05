5122256a2b54d359041ab296649007a55ba8bd2e
Haicheng Charles Zhao <czhao39@gmail.com>
2017-11-04 23:16:24 -0400

    Make index page look nicer

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
+    runAnimationSequence();
+
     $("#search-form").submit(function(event) {
         start_loader();
     });
 });
 
+
+function runAnimationSequence() {
+    $(".animation-group").velocity("transition.slideUpIn", { duration: 1000, stagger: 800 })
+}
 
 function start_loader() {
     $(".preloader-wrapper").addClass("active");
